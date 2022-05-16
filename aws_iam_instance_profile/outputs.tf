91e21fc7a565dd34e51172cf5689323c6232e93e
Damian Tykalowski <damian@d47zm3.me>
2022-05-16 17:18:23 +0200

    added aws instance profile outputs

---
+++ aws_iam_instance_profile/outputs.tf
@@ -0,0 +1,11 @@
+output "name" {
+  value = aws_iam_instance_profile.this.name
+}
+
+output "arn" {
+  value = aws_iam_instance_profile.this.name
+}
+
+output "id" {
+  value = aws_iam_instance_profile.this.id
+}
