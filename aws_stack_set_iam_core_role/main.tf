5b8afc20b71574b048b3e409734a4d66e016b8af
Damian Tykalowski <damian@d47zm3.me>
2023-02-22 09:12:14 +0100

    iam stackset role

--- aws_stack_set_iam_core_role/main.tf
+++ aws_stack_set_iam_core_role/main.tf
@@ -1,19 +1,19 @@
-data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
-
-    principals {
-      identifiers = ["cloudformation.amazonaws.com"]
-      type        = "Service"
-    }
-  }
-}
-
-resource "aws_iam_role" "AWSCloudFormationStackSetAdministrationRole" {
-  assume_role_policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_assume_role_policy.json
-  name               = "AWSCloudFormationStackSetAdministrationRole"
-}
+#data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_assume_role_policy" {
+#  statement {
+#    actions = ["sts:AssumeRole"]
+#    effect  = "Allow"
+#
+#    principals {
+#      identifiers = ["cloudformation.amazonaws.com"]
+#      type        = "Service"
+#    }
+#  }
+#}
+#
+#resource "aws_iam_role" "AWSCloudFormationStackSetAdministrationRole" {
+# assume_role_policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_assume_role_policy.json
+#  name               = "AWSCloudFormationStackSetAdministrationRole"
+#}
 
 resource "aws_cloudformation_stack_set" "iam_role" {
   #administration_role_arn = aws_iam_role.AWSCloudFormationStackSetAdministrationRole.arn
@@ -38,16 +38,25 @@ resource "aws_cloudformation_stack_set" "iam_role" {
   template_body = templatefile("template.json", { aws_trusted_entity = var.aws_trusted_entity })
 }
 
-data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy" {
-  statement {
-    actions   = ["sts:AssumeRole"]
-    effect    = "Allow"
-    resources = ["arn:aws:iam::*:role/${aws_cloudformation_stack_set.iam_role.execution_role_name}"]
+resource "aws_cloudformation_stack_set_instance" "example" {
+  deployment_targets {
+    organizational_unit_ids = [var.aws_organization_root_id]
   }
+
+  region         = var.region
+  stack_set_name = aws_cloudformation_stack_set.iam_role.name
 }
 
-resource "aws_iam_role_policy" "AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy" {
-  name   = "ExecutionPolicy"
-  policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy.json
-  role   = aws_iam_role.AWSCloudFormationStackSetAdministrationRole.name
-}
+#data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy" {
+#  statement {
+#    actions   = ["sts:AssumeRole"]
+#    effect    = "Allow"
+#    resources = ["arn:aws:iam::*:role/${aws_cloudformation_stack_set.iam_role.execution_role_name}"]
+#  }
+#}
+#
+#resource "aws_iam_role_policy" "AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy" {
+#  name   = "ExecutionPolicy"
+#  policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy.json
+#  role   = aws_iam_role.AWSCloudFormationStackSetAdministrationRole.name
+#}
--- aws_stack_set_iam_core_role/variables.tf
+++ aws_stack_set_iam_core_role/variables.tf
@@ -6,3 +6,12 @@ variable "name" {
 variable "aws_trusted_entity" {
   type = string
 }
+
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "aws_organization_root_id" {
+  type = string
+}
